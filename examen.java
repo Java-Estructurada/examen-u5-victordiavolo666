57b6f7d8d1cb50a1196e30c0d29499fcc075cc3f
azulcrema0789@gmail.com <azulcrema0789@gmail.com>
2026-05-27 08:12:19 -0600

    .

--- examen.java
+++ examen.java
@@ -4,6 +4,17 @@ public class examen{
     int [][] cSemanas ={ {120,135,110,140,150},
                         {90,105,100,115,120},
                         {150,160,155,170,165}};
+        int[] lHalcones = cSemanas[0];
+        int totalHalcones = calcularTotalSemana(lHalcones);
+        System.out.println("Total Halcones: " + totalHalcones);
     }
-    public static int calcularTotalSemana(int)
+        public static int calcularTotalSemana(int[] arreglo){
+        //int [] lHalcones={120,135,110,140,150,};
+        int acumulador =0;
+        for(int i=0;i<arreglo.length;i++){
+        acumulador = acumulador + arreglo[i];
+}
+        return acumulador;
+    }
+    public static 
 }
