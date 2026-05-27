b92bde97039301dfcdbe72e35f718a2f1a56dd97
azulcrema0789@gmail.com <azulcrema0789@gmail.com>
2026-05-27 07:46:02 -0600

    .

---
+++ examen.java
@@ -0,0 +1,9 @@
+public class examen{
+    public static void main(String[]args){
+    String [] nCuadrillas = {"Los Halcones","Cuadrilla Norte","Fuerza Verde"};
+    int [][] cSemanas ={ {120,135,110,140,150},
+                        {90,105,100,115,120},
+                        {150,160,155,170,165}};
+    }
+    public static int calcularTotalSemana(int)
+}
